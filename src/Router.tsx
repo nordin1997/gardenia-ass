36c016040e0f69f39c1e1c6ac5e46c3917c897bb
nordin <nordin.zahari@swiftlogistics.com.my>
2023-02-11 14:29:58 +0800

    added supertoken auth

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import * as reactRouterDom from 'react-router-dom'
+import { getSuperTokensRoutesForReactRouterDom } from 'supertokens-auth-react'
+
 import Home from 'app/pages/Home'
 import Issues from 'app/pages/Issues'
 import IssuesUpdate from 'app/pages/IssuesUpdate'
 
 const Router = () => {
   return (
-    <div className='w-full h-full'>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/issues' element={<Issues/>}/>
-        <Route path='/issues/new' element={<></>}/>
-        <Route path='/issues/:id' element={<IssuesUpdate/>}/>
-      </Routes>
-    </BrowserRouter>
-    </div>
+        <div className='w-full h-full'>
+          <BrowserRouter>
+            <Routes>
+              {getSuperTokensRoutesForReactRouterDom(reactRouterDom)}
+              <Route path='/' element={<Home/>}/>
+              <Route path='/issues' element={<Issues/>}/>
+              <Route path='/issues/new' element={<></>}/>
+              <Route path='/issues/:id' element={<IssuesUpdate/>}/>
+            </Routes>
+          </BrowserRouter>
+        </div>
   )
 }
 
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import Router from '../Router'
 import './App.css'
+import SuperTokens, { SuperTokensWrapper } from "supertokens-auth-react";
+import EmailPassword from "supertokens-auth-react/recipe/emailpassword";
+import Session from "supertokens-auth-react/recipe/session";
 
 function App() {
+  SuperTokens.init({
+    appInfo: {
+        // learn more about this on https://supertokens.com/docs/emailpassword/appinfo
+        appName: "Gardenia Issue Management",
+        apiDomain: 'http://localhost:4000',
+        websiteDomain: 'http://localhost:3000',
+        apiBasePath: "/auth",
+        websiteBasePath: "/auth",
+    },
+    recipeList: [
+        EmailPassword.init(),
+        Session.init()
+    ]
+});
   return (
-    <Router/>
+    <SuperTokensWrapper>
+      <Router/>
+    </SuperTokensWrapper>
   );
 }
 
